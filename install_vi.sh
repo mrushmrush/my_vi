96b52014adc7045842fa1392b1efa93343b72720
Mark Rush <mrush@mindspring.com>
2021-03-10 14:28:55 -0800

    added vim ancilliary files in tarball for installation

--- install_vi.sh
+++ install_vi.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
 #to be run from inside repository ~/my_vi
+
+#copy vim.bin to 
+#change etc/alternate links to 
+#mkdir /usr/local/share/vim
+#cp tar to /usr/local/share/vim
+#extract tarball
